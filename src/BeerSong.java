614f18598731dc3cd2dd1c80a981c4ba5c5ff218
Boban Ljuljdjurovic <bobanstudent@gmail.com>
2016-09-30 14:03:47 -0400

    Added a call to the 99Bottles method in the main method

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -4,9 +4,11 @@
 public class BeerSong {
     public static void main(String[] args){
         //This will call the Ninety_Nine_Bottles_of_Beer
+        Ninety_Nine_Bottles_of_Beer();
     }
 
     public static void Ninety_Nine_Bottles_of_Beer(){
         //This will have a loop of the song 99 bottles of beer. It will loop from 99 to 0. 0 bottles will display no more bottles of beer.
+
     }
 }
