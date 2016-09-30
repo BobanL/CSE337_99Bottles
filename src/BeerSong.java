eee1f4cfdd8909ae06deb9cc7cf89701c075b920
Boban Ljuljdjurovic <bobanstudent@gmail.com>
2016-09-30 13:49:43 -0400

    Created the java file Beersong.java. I have initialized the methods inside the class and added comments of what each one will be doing.

---
+++ src/BeerSong.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Boban on 9/30/2016.
+ */
+public class BeerSong {
+    public static void main(String[] args){
+        //This will call the Ninety_Nine_Bottles_of_Beer
+    }
+
+    public static void Ninety_Nine_Bottles_of_Beer(){
+        //This will have a loop of the song 99 bottles of beer. It will loop from 99 to 0. 0 bottles will display no more bottles of beer.
+    }
+}
